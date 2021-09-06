45d9f8d6afb83db8fabdeb8afc12d3252cfbfeb4
Alexander Bliudenov <abludenov@gmail.com>
2021-09-07 09:47:15 +1000

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,38 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Toyota", 111111)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Lada", 222222)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Ford", 333333)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Honda", 444444)));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getUserCar());
+            System.out.println();
+        }
+
+        System.out.println(userService.getUserByCar("Toyota", 111111));
+
+        context.close();
+    }
+}
